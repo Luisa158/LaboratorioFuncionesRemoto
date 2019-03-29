2816d677764e89b49c9a8527ff9c4ba571c0f799
Luisa158 <luisa.monsalve.2019@upb.edu.co>
2019-03-29 11:38:58 -0500

    Introduje un contador

--- power.py
+++ power.py
@@ -9,6 +9,11 @@ def a_power_b (a,b):
     return cont
 
 
+error = 0
+poten = 0
+par = 0
+impar = 0
+
 while True:
     try:
         a=int(input("Por favor, ingrese el número base: "))
@@ -17,11 +22,24 @@ while True:
             break
 
         b=int(input("Por favor, ingrese el número de la potencia: "))
+        poten=poten+1
 
         res= a_power_b(a,b)
         print("El resultado es: ", res)
+        if res%2==0:
+            par=par+1
+        else:
+            impar=impar+1
 
     except:
         print("TIENE UN ERROR!!")
-    
+        error=error+1
+
+
+print("El número de potencias calculadas es: ", poten)
+print("El número de erroes es: ", error)
+print("El número de resultados pares fue: ", par)
+print("El número de resultados impares fue: ", impar)
+
+
 
