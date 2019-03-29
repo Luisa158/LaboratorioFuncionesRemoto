7067bd4f8d4a1118ff641a59ed61dc88d6887a87
Luisa158 <luisa.monsalve.2019@upb.edu.co>
2019-03-29 10:49:05 -0500

    Pausa en cero

--- power.py
+++ power.py
@@ -3,9 +3,17 @@ a=int(input("Por favor, ingrese el número base: "))
 b=int(input("Por favor, ingrese el número de la potencia: "))
 
 def a_power_b (a,b):
-    cont=0
-    for i in range (1,b+1):
-      cont=a*i
-    print("El resultado es: ", cont)
+
+    while True:
+        if a!=0:
+            cont=0
+            for i in range (1,b+1):
+                cont=a*i
+
+            print("El resultado es: ", cont)
+            a = int(input("Por favor, ingrese el número base: "))
+            b = int(input("Por favor, ingrese el número de la potencia: "))
+        else:
+            break
 
 a_power_b(a,b)
