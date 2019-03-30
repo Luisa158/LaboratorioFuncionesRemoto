b4343f4bfe21c91b6d73f8fe10be1ee343b97557
Luisa158 <luisa.monsalve.2019@upb.edu.co>
2019-03-30 17:20:37 -0500

    Retorne

--- primo.py
+++ primo.py
@@ -1,21 +1,40 @@
 
-a=int(input("Por favor, introduca el número a evaluar: "))
-
-
 def is_prime(a):
     contador=0
-    for i in range (2,a):
+    for i in range (2,a+1):
         if a % i == 0:
             contador+=1
-            print("Divisor: ", i)
+            print("Divider: ", i)
 
-    if contador>0:
-         print("Is NOT a prime number")
+    if contador > 2:
+        no_prime=0
+        return no_prime
 
     else:
-         print("Is a primer number")
+        prime=1
+        return prime
 
-is_prime(a)
+
+while True:
+    try:
+        a = int(input("Por favor, introduca el número a evaluar: "))
+        if a==0:
+            break
+        if a<0:
+            break
+        if a==1:
+            print(0)
+
+        res=is_prime(a)
+        if res==0:
+            print(0)
+        else:
+            res==1
+            print(1)
+    except:
+        print(-1)
+        print("Hay un ERROR!!")
+
 
 
 
